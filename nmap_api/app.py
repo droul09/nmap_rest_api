fd3a87bd55c24cce44ba0c3aff5f36320fcb83da
David Rouleau <rouleau09@gmail.com>
2023-03-17 13:23:29 -0400

    Building out the scan api functionality and containerization

--- nmap_api/app.py
+++ nmap_api/app.py
@@ -1,59 +1,68 @@
 #!/usr/bin/env python3
 
 from nmap import PortScanner
-from flask import Flask, request
-from pymongo import MongoClient
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+import socket
 
 # https://xael.org/pages/python-nmap-en.html
+# https://nmap.org/book/man-port-specification.html
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
-db = client['nmap_scans']
-collection = db['scans']
+app.config["MONGO_URI"] = "mongodb://mongo:27017/dev"
+mongo = PyMongo(app)
+db = mongo.db
+
+@app.route("/")
+def index():
+    hostname = socket.gethostname()
+    return jsonify(
+        message="Welcome to the NMAP API app service!\
+        This service is intended to scan, process, and store nmap results.\
+        I am running inside {} pod!".format(hostname)
+    )
 
 @app.route('/scan', methods=['POST'])
-def nmap_scan(host):
+def nmap_scan():
     ''' Initiate a scan of an IP address or hostname, can be multiple'''
-    #hosts = request.json['hosts']
+    host = request.form['host']
     nm = PortScanner()
-    r = nm.scan(host, arguments='-T4')
-    #print(r)
-    response = {}
-    for host in r['scan'].items():
+    data = nm.scan(host, arguments='-T4')
+
+    aggregate = {"hosts": []}
+    for host in data['scan'].items():
+        response = {}
         response["host"] = host[1]['hostnames'][0]['name']
         response["ip"] = host[0]
         response["status"] = host[1]['status']['state'] 
+        response["protocol"] = list(host[1].keys())[-1]
         for port in host[1]['tcp'].items():
-            response["port"] = {port[0]:port[1]['state']}
-    print(response)
+            if response.get("ports"):
+                response["ports"] += [{str(port[0]):port[1]['state']}]
+            else:
+                response["ports"] = [{str(port[0]):port[1]['state']}]
+        aggregate['hosts'].append(response)
+        db.nmap_scan_results.insert_one(response)
 
-
+    aggregate['scanstats'] = data['nmap']['scanstats']
 
     
-    #print(nm.csv())
-    # write the scan result to the MongoDB instance
-    #collection.insert_one(result)
-    return 'Scan results saved to the database'
+
+    return jsonify(aggregate)
 
 @app.route('/get_scans', methods=['GET'])
 def get_scan():
-    ''' Makes a call to the MongoDB database to retrieve the latest scans'''
-    results = []
-    for scan in collection.find():
-        results.append(scan)
-    return str(results)
+    ''' Makes a call to the MongoDB database to retrieve the latest scans '''
+    return True
+    
 
 @app.route('/get_changes', methods=['GET'])
 def get_scan_changes():
     ''' Makes a call to the MongoDB database to retrieve the last two scans
     and does a difference operation to find deltas'''
-    results = []
-    scans = list(collection.find().sort("_id", -1).limit(2))
-    if len(scans) == 2:
-        delta = set(scans[1]['scan']) - set(scans[0]['scan'])
-        results.append({'delta': list(delta)})
-    return str(results)
+    return True
+    
 
 if __name__ == '__main__':
-    nmap_scan('scanme.nmap.org')
-    #app.run(debug=True)
+    #nmap_scan('scanme.nmap.org 127.0.0.1 8.8.8.8')
+    app.run(host='0.0.0.0', port=5000, debug=True)
